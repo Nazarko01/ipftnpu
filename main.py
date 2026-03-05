df6da18fb7fc89d604e48bd7a03772e49a432766
Nazar <kozlinskyi.n@gmail.com>
2026-03-05 12:30:46 +0200

    Веб-платформа для рецептів.

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Hello world")
+recipes = []
+
+while True:
+    print("\n1 - Додати рецепт")
+    print("2 - Показати рецепти")
+    print("3 - Вихід")
+
+    choice = input("Оберіть дію: ")
+
+    if choice == "1":
+        name = input("Назва рецепта: ")
+        ingredients = input("Інгредієнти: ")
+        recipes.append({"name": name, "ingredients": ingredients})
+        print("Рецепт додано!")
+
+    elif choice == "2":
+        if len(recipes) == 0:
+            print("Рецептів немає")
+        else:
+            for r in recipes:
+                print("Назва:", r["name"])
+                print("Інгредієнти:", r["ingredients"])
+                print()
+
+    elif choice == "3":
+        break
+
+    else:
+        print("Невірна команда")
